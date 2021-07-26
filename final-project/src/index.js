8c22f48209e35cecf676739a4c17b6035e50f3c9
Heesung jang <heesungj7@gmail.com>
2021-07-27 00:23:05 +0900

    [refactor] index.js 리덕스 스토어 경로 변경

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { store } from "./Redux/configureStore";
+import { store } from "./redux/configureStore";
 import { Provider } from "react-redux";
 
 import App from "./App";
