2e039bd2bc052bc97e01ccdab3e4cc4b1a64c249
Heesung jang <heesungj7@gmail.com>
2021-07-27 00:05:57 +0900

    [feat] index.js에 redux store 주입

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { store } from "./Redux/configureStore";
+import { Provider } from "react-redux";
+
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
-    document.getElementById("root")
+    document.getElementById("root"),
 );
