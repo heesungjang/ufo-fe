83420122a0c63c536b9c4d4b33da4491e43d6db6
Heesung jang <heesungj7@gmail.com>
2021-07-27 00:07:28 +0900

    [refactor] logger refactored

--- final-project/src/Utils/logger.js
+++ final-project/src/Utils/logger.js
@@ -1,6 +1,6 @@
 // 개발 모드에서만 console.log가 찍히는 logger
-export const logger = (msg) => {
-    if (process.env.REACT_APP_NODE_ENV === "production") {
+export const logger = msg => {
+    if (process.env.NODE_ENV === "production") {
         return;
     }
     console.log(msg);
