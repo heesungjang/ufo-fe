e84523b0ec64b4454d32a1069ddee334b0a571f8
Heesung jang <heesungj7@gmail.com>
2021-07-27 00:06:22 +0900

    [feat] 리덕스 스토어 생성

---
+++ final-project/src/redux/configureStore.js
@@ -0,0 +1,27 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+
+export const history = createBrowserHistory();
+
+//리듀서 전달하기
+const reducer = combineReducers({
+    router: connectRouter(history),
+});
+
+const middlewares = [];
+
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
+
+export const store = configureStore({
+    reducer,
+    middleware: [...middlewares, ...getDefaultMiddleware()],
+    devTools: env !== "production",
+});
