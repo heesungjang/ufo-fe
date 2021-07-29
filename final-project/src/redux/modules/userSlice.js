6153abe439188d1e41e57f0cb94a5b06262ac769
Heesung jang <heesungj7@gmail.com>
2021-07-29 16:27:45 +0900

    [feat] 로그인 회원가입 reducer 작업

--- final-project/src/redux/modules/userSlice.js
+++ final-project/src/redux/modules/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { signupUserDB } from "../async/user";
+import { signupUserDB, loginUserDB } from "../async/user";
 
 /**
  * @initialState 사용자 정보
@@ -8,29 +8,53 @@ import { signupUserDB } from "../async/user";
 
 const initialState = {
     user: {},
-    errorMessage: "",
     isFetching: false,
     isLoggedIn: false,
+    errorMessage: "",
+    isSignupSuccess: "",
 };
 
 const userSlice = createSlice({
     name: "user",
     initialState: initialState,
-    reducers: {},
+    reducers: {
+        //회원가입 성공, 로그인 창으로 이동후 회원가입 상태 초기화
+        resetSignupSuccess: state => {
+            state.isSignupSuccess = "";
+        },
+    },
     extraReducers: {
+        //┏---------------회원가입 Reducer-----------------------┓
         [signupUserDB.pending]: (state, action) => {
             state.isFetching = true;
         },
-        [signupUserDB.fulfilled]: (state, { payload: user }) => {
-            state.user = user;
+        [signupUserDB.fulfilled]: (state, { payload }) => {
             state.isFetching = false;
             state.errorMessage = "";
+            state.isSignupSuccess = payload;
         },
         [signupUserDB.rejected]: (state, { payload: errorMessage }) => {
             state.errorMessage = errorMessage;
             state.isFetching = false;
         },
+        //--------------------------------------------------------
+        //┏-----------------로그인 Reducer----------------------┓
+        [loginUserDB.pending]: (state, action) => {
+            state.isFetching = true;
+        },
+        [loginUserDB.fulfilled]: (state, { payload: user }) => {
+            state.isFetching = false;
+            state.user = user;
+            state.errorMessage = "";
+        },
+        [loginUserDB.rejected]: (state, { payload: errorMessage }) => {
+            state.isFetching = false;
+            state.errorMessage = errorMessage;
+        },
+        //--------------------------------------------------------
     },
 });
+
+export const { resetSignupSuccess } = userSlice.actions;
 
 export default userSlice;
