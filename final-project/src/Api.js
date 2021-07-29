d3a89c3c034a0b54235cea2a054152538e492beb
Heesung jang <heesungj7@gmail.com>
2021-07-29 16:23:12 +0900

    [feat] user Api 모듈 추가 + base url 설정

--- final-project/src/Api.js
+++ final-project/src/Api.js
@@ -1,23 +1,37 @@
 import axios from "axios";
-
-import { getToken } from "./token";
+import { getToken } from "./Utils/Utils";
 
 // Axios 인스턴스 설정
 const instance = axios.create({
-    baseURL: "<백엔드 서버 endpoint>",
+    baseURL: "http://3.36.90.60/",
 });
 
-// interceptor를 통한 header 설정
-instance.interceptors.request.use((config) => {
+//┏----------interceptor를 통한 header 설정----------┓
+instance.interceptors.request.use(async config => {
     config.headers["content-type"] = "application/json; charset=utf-8";
     config.headers["X-Requested-With"] = "XMLHttpRequest";
     config.headers["Accept"] = "*/*";
-    config.headers["authorization"] = `Bearer ${getToken()}`;
+    //getToken는 로컬 스토리지에 토큰이 있다면 반환한다.
+    config.headers["authorization"] = `Bearer ${await getToken()}`;
     return config;
 });
 
-//
-export const userApi = {};
+// 사용자 관련 axios API 통신
+export const userApi = {
+    signup: data =>
+        instance.post("api/user", {
+            email: data.email,
+            nickname: data.nickname,
+            password: data.password,
+        }),
+    login: data =>
+        instance.post("api/login", {
+            email: data.email,
+            password: data.password,
+        }),
+    getUser: userId => instance.get(`api/user/${userId}`),
+    kakaoLogin: () => instance.get("api/kakao"),
+};
 
 export const freeBoardApi = {};
 
