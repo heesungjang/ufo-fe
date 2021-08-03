48c987ba188abe1ffd6444002d7d22bef943abc5
Heesung jang <heesungj7@gmail.com>
2021-08-03 19:47:17 +0900

    [feat] 메인 페이지 전용 카테고리 추가

--- final-project/src/categories.js
+++ final-project/src/categories.js
@@ -10,6 +10,19 @@ const country = [
     { countryId: 5, countryName: "영국" },
 ];
 const freeCategory = [
+    { categoryId: "0", categoryName: "질문" },
+    { categoryId: 1, categoryName: "정보" },
+    { categoryId: 2, categoryName: "주거" },
+    { categoryId: 3, categoryName: "취업" },
+    { categoryId: 4, categoryName: "연애" },
+    { categoryId: 5, categoryName: "게임" },
+    { categoryId: 6, categoryName: "유머" },
+    { categoryId: 7, categoryName: "코로나" },
+    { categoryId: 8, categoryName: "장터" },
+    { categoryId: 9, categoryName: "취미" },
+    { categoryId: 10, categoryName: "기타" },
+];
+const freeCategoryForMainPage = [
     { categoryId: 0, categoryName: "질문" },
     { categoryId: 1, categoryName: "정보" },
     { categoryId: 2, categoryName: "주거" },
@@ -24,6 +37,6 @@ const freeCategory = [
 ];
 
 //카테고리를 작성하시고, 아래의 중괄호 안에 넣어주어야 사용할 수 있어요!
-const categories = { country, freeCategory };
+const categories = { country, freeCategory, freeCategoryForMainPage };
 
 export default categories;
