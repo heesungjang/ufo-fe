c7332d7e02a01b8feeae7f0ee6672fca13cddc30
Heesung jang <heesungj7@gmail.com>
2021-08-03 16:56:28 +0900

    [feat] 메인 페이지 와이어프레임 작업

--- final-project/src/Pages/Home.js
+++ final-project/src/Pages/Home.js
@@ -4,26 +4,68 @@ import SearchBox from "../Components/SearchBox";
 import { useDispatch, useSelector } from "react-redux";
 import { getFreeListDB } from "../redux/async/freeBoard";
 import BoardBox from "../Components/BoardBox";
+import { getUnivBoardDB } from "../redux/async/univBoardAsync";
+import categories from "../categories";
 
 const Home = () => {
     const dispatch = useDispatch();
+
+    //--- 자유 게시판 게시물 리덕스 스토어 구독--------
     const freeBoardPostList = useSelector(state => state.freeBoard.list);
+    //----
+
+    // 학교 게시판 게시물 리덕스 스토어 구독------------
+    const univBoardPostList = useSelector(state => state.univBoard.list);
+    //----
+
+    // 자유 게시판 태그
+
+    // 학교 게시판 / 자유 게시판 thunk dispatch--------
     useEffect(() => {
         dispatch(getFreeListDB());
+        dispatch(getUnivBoardDB());
     }, []);
+    //----
+
     return (
         <HomeContainer>
             <SearchContainer>
-                <SearchBox
-                    freeBoardPostList={freeBoardPostList && freeBoardPostList}
-                />
+                <SearchBox />
             </SearchContainer>
             <BoardContainer>
-                <BoardBox />
-                <BoardBox />
-                <BoardBox />
-                <BoardBox />
-                <BoardBox />
+                <BoardBox
+                    title="인기 게시판"
+                    postList={
+                        freeBoardPostList && freeBoardPostList.slice(0, 5)
+                    }
+                />
+                <BoardBox
+                    title="학교 게시판"
+                    postList={
+                        univBoardPostList && univBoardPostList.slice(0, 5)
+                    }
+                />
+                <BoardBox
+                    title="자유 게시판"
+                    postList={
+                        freeBoardPostList && freeBoardPostList.slice(0, 5)
+                    }
+                />
+                {freeBoardPostList &&
+                    categories.freeCategory.map((category, idx) => {
+                        return (
+                            <BoardBox
+                                tag={category.categoryName}
+                                postList={freeBoardPostList
+                                    .filter(
+                                        post =>
+                                            post.category ===
+                                            category.categoryId,
+                                    )
+                                    .slice(0, 5)}
+                            />
+                        );
+                    })}
             </BoardContainer>
         </HomeContainer>
     );
