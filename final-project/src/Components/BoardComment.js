94d98b15724357484d77894b211c477189c2bac7
che-ri <rnjs1135@gmail.com>
2021-08-07 05:56:45 +0900

    [feat] 댓글리스트 모듈화 작업 중 자유게시판 CUD 확인 완료

--- final-project/src/Components/BoardComment.js
+++ final-project/src/Components/BoardComment.js
@@ -9,7 +9,6 @@ import {
 } from "../redux/async/freeBoard";
 
 import {
-    getCommentDB,
     addUniBoardCommentDB,
     editUniBoardCommentDB,
     deleteUniBoardCommentDB,
@@ -19,8 +18,8 @@ import {
  * @author kwonjiyeong
  * @param post: 포스트정보, boardName: 게시판이름
  * @returns 게시판 디테일페이지 댓글리스트 뷰
- * @역할 게시판 디테일페이지 댓글리스트 뷰 렌더링
- * @필수값 postId, boardName
+ * @역할 게시판 디테일페이지 댓글리스트 뷰 렌더링, 댓글 CRUD 기능 중 CR
+ * @필수값 postId : 포스트아이디, boardName : 자유게시판이면 freeboard이고 대학게시판이면 univboard, commentList : 댓글리스트
  */
 const BoardComment = ({ commentList, boardName, postId }) => {
     const dispatch = useDispatch();
@@ -29,21 +28,27 @@ const BoardComment = ({ commentList, boardName, postId }) => {
     const [content, setContent] = useState(""); //댓글 입력값을 넣을 공간
 
     const addComment = () => {
-        //서버에 필요한 정보를 정리하고, 댓글을 추가하는 미들웨어 함수로 보낸다.
+        //서버에 필요한 정보를 정리하고, 댓글을 추가하는 미들웨어 함수로 보내줍니다.
         if (user.user_id === undefined) return alert("로그인을 해주세요!"); //유저정보가 없으면 return 합니다.
         if (user.user_id && !content) return alert("댓글을 입력해주세요!");
-
-        if (boardName === "univboard") {
-            if (!user.univ_id)
-                return alert("마이페이지에서 대학 인증을 해주세요!");
-        }
 
         const req = {
             post_id: postId,
             content: content,
         };
 
-        dispatch(addFreeCommentDB(req));
+        if (boardName === "freeboard") {
+            //자유게시판과 연결되는 미들웨어함수로 보내줍니다.
+            dispatch(addFreeCommentDB(req));
+        }
+
+        if (boardName === "univboard") {
+            //대학게시판과 연결되는 미들웨어함수로 보내줍니다.
+            if (!user.univ_id)
+                return alert("마이페이지에서 대학 인증을 해주세요!");
+            dispatch(addUniBoardCommentDB(req));
+        }
+
         setContent(""); //댓글을 추가하고, 댓글입력칸은 지워줍니다!
     };
 
@@ -63,27 +68,21 @@ const BoardComment = ({ commentList, boardName, postId }) => {
             {/* 자유게시판일 때 렌더링 */}
             {commentList && (
                 <>
-                    <h3>댓글 {commentList.length}개</h3>
-                    <CommentTable>
+                    <CommentBox>
                         {/* 댓글의 목록을 나타내는 컴포넌트입니다. */}
-                        <TableRow>
-                            {/* 댓글의 헤더가 들어가는 공간입니다. */}
-                            <div>
-                                <span>작성자</span>
-                            </div>
-                            <div>
-                                <span>내용</span>
-                            </div>
-                            <div></div>
-                        </TableRow>
+                        <CommentCnt>
+                            <span>댓글 {commentList.length}개</span>
+                        </CommentCnt>
                         {commentList &&
                             commentList.map(comment => (
-                                <TableRow key={comment.comment_id}>
-                                    {/* 각 댓글의 데이터들이 들어가는 공간입니다. */}
-                                    <TableContent {...comment} />
-                                </TableRow>
+                                // 각 댓글의 데이터들이 들어가는 공간입니다.
+                                <Comment
+                                    key={comment.comment_id}
+                                    comment={comment}
+                                    boardName={boardName}
+                                />
                             ))}
-                    </CommentTable>
+                    </CommentBox>
                 </>
             )}
         </BoardCommentContainer>
@@ -103,52 +102,36 @@ const CommentWrite = styled.div`
     padding-bottom: 10px;
     input {
         all: unset;
+        width: 95%;
     }
     button {
+        background: white;
         padding: 0 10px;
         border-radius: 10px;
     }
 `;
 
-const CommentTable = styled.div`
-    border: 2px solid gray;
-    > div:not(:last-child) {
-        border-bottom: 1px solid gray;
-    }
-`;
+const CommentBox = styled.div``;
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
-    > div {
-        padding: 10px;
-    }
-    > div:first-child {
-        border-right: 1px solid gray;
-    }
-    > div:last-child {
-        display: flex;
-        justify-content: flex-end;
-        button {
-            margin-left: 10px;
-        }
-    }
+const CommentCnt = styled.div`
+    color: #707070;
+    margin-bottom: 20px;
 `;
 
 /**
  * @author kwonjiyeong
- * @param props : {history, location, match}
+ * @param props : 댓글 리스트 중 댓글에 한 개에 대한 정보가 들어있다.
  * @returns 자유게시판 특정 게시물의 특정 댓글 뷰
  * @역할 자유게시판 특정 게시물의 특정 댓글 뷰 렌더링, 댓글 CRUD 기능 중 UD
  * @필수값 props (부모컴포넌트로부터 하나의 댓글 정보가 담겨져서 온다.)
  */
 
-const TableContent = props => {
+const Comment = ({ comment, boardName }) => {
     const dispatch = useDispatch();
     const [isEdit, setIsEdit] = useState(false); //수정모드인지 아닌지 판별해주는 스위치입니다.
-    const [content, setContent] = useState(props.content); //댓글 입력값을 저장할 곳입니다.
+    const [content, setContent] = useState(comment.content); //댓글 입력값을 저장할 곳입니다.
     const user = useSelector(state => state.user.user); //유저정보
-    const isAuthor = user.user_id === props.user_id ? true : false; //댓글의 작성자 인지 아닌지 판별해주는 값
+    const isAuthor = user.user_id === comment.user_id ? true : false; //댓글의 작성자 인지 아닌지 판별해주는 값
 
     const clickEditBtn = () => {
         //isEdit가 false가 되면 text가 나타나고, true면 input이 나타나게 하는 스위치작동함수
@@ -157,17 +140,29 @@ const TableContent = props => {
 
     const cancelEdit = () => {
         //수정을 하다가 취소버튼을 누를 때, 사용하는 기능
-        setContent(props.content);
+        setContent(comment.content);
         setIsEdit(false);
     };
 
     const editComment = () => {
         //서버로 보낼 데이터를 정리하여, 댓글을 수정하는 미들웨어함수로 보내줍니다.
         const req = {
-            comment_id: props.comment_id,
+            comment_id: comment.comment_id,
             content: content,
         };
-        dispatch(editFreeCommentDB(req));
+
+        if (boardName === "freeboard") {
+            //자유게시판과 연결되는 미들웨어함수로 보내줍니다.
+            dispatch(editFreeCommentDB(req));
+        }
+
+        if (boardName === "univboard") {
+            //대학게시판과 연결되는 미들웨어함수로 보내줍니다.
+            if (!user.univ_id)
+                return alert("마이페이지에서 대학 인증을 해주세요!");
+            dispatch(editUniBoardCommentDB(req));
+        }
+
         setIsEdit(false);
         setContent(content);
     };
@@ -175,51 +170,120 @@ const TableContent = props => {
     const deleteComment = () => {
         //서버로 보낼 데이터를 정리하여, 댓글을 삭제하는 미들웨어함수로 보내줍니다.
         const req = {
-            comment_id: props.comment_id,
+            comment_id: comment.comment_id,
         };
-        dispatch(deleteFreeCommentDB(req));
+
+        if (boardName === "freeboard") {
+            //자유게시판과 연결되는 미들웨어함수로 보내줍니다.
+            dispatch(deleteFreeCommentDB(req));
+        }
+
+        if (boardName === "univboard") {
+            //대학게시판과 연결되는 미들웨어함수로 보내줍니다.
+            if (!user.univ_id)
+                return alert("마이페이지에서 대학 인증을 해주세요!");
+            dispatch(deleteUniBoardCommentDB(req));
+        }
+
         setIsEdit(false);
-        setContent(content);
     };
 
     return (
         <>
-            <div>
-                <span>{props.user.nickname}</span>
-            </div>
-            <div>
-                {/* 수정모드면 input이 나타나고, 아니면 text가 나타납니다. */}
-                {isEdit ? (
-                    <input
-                        type="text"
-                        value={content}
-                        onChange={e => setContent(e.target.value)}
+            <CommentContainer>
+                <Header>
+                    {/* 유저이미지 */}
+                    <img
+                        src={require("../assets/pngegg.png").default}
+                        alt="user"
                     />
-                ) : (
-                    <span>{props.content}</span>
-                )}
-            </div>
-            <div>
-                {/* 댓글의 작성자가 맞으면 버튼들이 나타납니다. */}
-                {isAuthor && (
-                    <>
-                        {/* 수정모드면 취소,저장버튼이 나타나고, 아니면 수정,삭제버튼이 나타납니다. */}
-                        {isEdit ? (
+
+                    {/* 유저닉네임 */}
+                    <span>{comment.user.nickname}</span>
+
+                    {/* 현재시간과 댓글생성시간과 비교한 시간 (지금은 댓글생성시간으로 표기됨) */}
+                    <span>{comment.createdAt}</span>
+
+                    <Controls>
+                        {/* 댓글의 작성자가 아니면 답글버튼이 나타납니다. */}
+                        {!isAuthor && <button onClick={() => {}}>답글</button>}
+
+                        {/* 댓글의 작성자가 맞으면 아래의 버튼들이 나타납니다. */}
+                        {isAuthor && (
                             <>
-                                <button onClick={cancelEdit}>취소</button>
-                                <button onClick={editComment}>저장</button>
-                            </>
-                        ) : (
-                            <>
-                                <button onClick={clickEditBtn}>수정</button>
-                                <button onClick={deleteComment}>삭제</button>
+                                {/* 수정모드면 취소,저장버튼이 나타나고, 아니면 수정,삭제버튼이 나타납니다. */}
+                                {isEdit ? (
+                                    <>
+                                        <button onClick={cancelEdit}>
+                                            취소
+                                        </button>
+                                        <button onClick={editComment}>
+                                            저장
+                                        </button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <button onClick={clickEditBtn}>
+                                            수정
+                                        </button>
+                                        <button onClick={deleteComment}>
+                                            삭제
+                                        </button>
+                                    </>
+                                )}
                             </>
                         )}
-                    </>
-                )}
-            </div>
+                    </Controls>
+                </Header>
+                <Content>
+                    {/* 수정모드면 input이 나타나고, 아니면 text가 나타납니다. */}
+                    {isEdit ? (
+                        <input
+                            type="text"
+                            value={content}
+                            onChange={e => setContent(e.target.value)}
+                        />
+                    ) : (
+                        <span>{comment.content}</span>
+                    )}
+                </Content>
+            </CommentContainer>
         </>
     );
 };
+
+const CommentContainer = styled.div`
+    padding-bottom: 20px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    height: 30px;
+    > * {
+        height: 100%;
+    }
+    > :not(:last-child) {
+        margin-right: 10px;
+    }
+`;
+
+const Controls = styled.div`
+    button {
+        padding: 0 10px;
+        border-radius: 10px;
+        background: white;
+    }
+    button:not(:last-child) {
+        margin-right: 10px;
+    }
+`;
+const Content = styled.div`
+    input {
+        all: unset;
+        border-bottom: 2px solid black;
+        width: 30%;
+    }
+`;
 
 export default BoardComment;
