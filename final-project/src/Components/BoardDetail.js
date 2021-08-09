24a0896bcbdca9a7ed8583c179360cc03a100dce
Heesung jang <heesungj7@gmail.com>
2021-08-09 11:58:45 +0900

    [feat] 게시물 열람시 조회수 증가 추가
    쿠키 설정을 통해 20분 간격으로 증가

--- final-project/src/Components/BoardDetail.js
+++ final-project/src/Components/BoardDetail.js
@@ -16,10 +16,22 @@ import {
 } from "../redux/async/univBoard";
 
 import { BiHeart } from "react-icons/bi";
+import { Button as Mbutton } from "@material-ui/core";
 import { MdComment } from "react-icons/md";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LinkIcon from "@material-ui/icons/Link";
 
 import categories from "../categories";
 import VisibilityIcon from "@material-ui/icons/Visibility";
+
+import TimeCounting from "time-counting";
+import moment from "moment";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useCookies } from "react-cookie";
+import instance from "../api";
 
 const BoardDetail = ({ page }) => {
     const dispatch = useDispatch();
@@ -34,12 +46,21 @@ const BoardDetail = ({ page }) => {
     // 유저 아이디 스토어 구독
     const userId = useSelector(state => state.user.user.user_id);
 
-    //댓글목록
-    const commentList = useSelector(state =>
-        page === "freeboard"
-            ? state.freeBoard.commentList
-            : state.univBoard.commentList,
-    );
+    //작성 시간 config 설정
+    const timeOption = {
+        lang: "ko",
+        // objectTime: "2020-08-10 06:00:00",
+        objectTime: moment().format(`YYYY-MM-DD HH:mm:ss`),
+        calculate: {
+            justNow: 61,
+        },
+    };
+
+    //-------------조회수--------------
+    let now = new Date();
+    let after20m = new Date();
+    const viewCookie = page === "freeboard" ? `f${postId}` : `u${postId}`;
+    const [cookies, setCookie, removeCookie] = useCookies([viewCookie]);
 
     useEffect(() => {
         dispatch(
@@ -52,6 +73,26 @@ const BoardDetail = ({ page }) => {
                 ? getFreeCommentListDB(postId)
                 : getUnivBoardCommentDB(postId),
         ); //특정게시물의 댓글목록 가져오는 함수
+        const callView = async () => {
+            if (page === "freeboard") {
+                await instance.get(`free/post/${postId}/view_count`);
+            } else {
+                await instance.get(`univ/post/${postId}/view_count`);
+            }
+        };
+        // 쿠키 설정을 통해서 조회수 증가는 20분으로 제한한다.
+        if (page === "freeboard" && cookies.viewCookie !== `f${postId}`) {
+            after20m.setMinutes(now.getMinutes() + 20);
+            setCookie("viewCookie", viewCookie, { expires: after20m });
+            callView();
+        } else if (
+            page === "univboard" &&
+            cookies.viewCookie !== `u${postId}`
+        ) {
+            after20m.setMinutes(now.getMinutes() + 20);
+            setCookie("viewCookie", viewCookie, { expires: after20m });
+            callView();
+        }
     }, [dispatch, postId, page]);
 
     //서버에 필요한 정보를 정리하고, 포스트를 삭제하는 미들웨어 함수로 보낸다.
@@ -65,6 +106,16 @@ const BoardDetail = ({ page }) => {
                 ? deleteFreePostDB(req)
                 : deleteUnivBoardPostDB(req),
         );
+    };
+
+    const copyUrl = () => {
+        const el = document.createElement("input");
+        el.value = window.location.href;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+        document.body.removeChild(el);
+        toast("게시물 링크가 클립보드에 복사되었습니다!");
     };
 
     return (
@@ -86,17 +137,26 @@ const BoardDetail = ({ page }) => {
                         {post && post.user && post.user.nickname}
                     </Nickname>
                     <IconContainer>
+                        <ToastContainer />
+
+                        <Mbutton onClick={copyUrl}>
+                            <Icon>
+                                <LinkIcon />
+                            </Icon>
+                        </Mbutton>
+
                         <Icon>
                             <BiHeart />
                             <span>5개</span>
                         </Icon>
                         <Icon>
-                            <MdComment />
-                            <span>{commentList && commentList.length}</span>
-                        </Icon>
-                        <Icon>
                             <VisibilityIcon />
-                            {/* <span>{post.view_count}</span> */}
+                            <span>{post && post.view_count}</span>
+                        </Icon>
+
+                        <Icon>
+                            <AccessTimeIcon />
+                            {TimeCounting(post && post.createdAt, timeOption)}
                         </Icon>
                     </IconContainer>
                 </NicknameIconContainer>
@@ -111,7 +171,7 @@ const BoardDetail = ({ page }) => {
             </ContentBodyContainer>
             <ButtonContainer>
                 <Button onClick={() => history.push(`/${page}`)}>목록</Button>
-                {userId && post && userId === post.user && post.user.user_id && (
+                {userId && post && post.user && userId === post.user.user_id && (
                     <>
                         <Button
                             onClick={() =>
