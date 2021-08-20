8d655069f9df309dee65764d5a018a7ecf922285
Heesung jang <heesungj7@gmail.com>
2021-08-20 10:54:34 +0900

    [refactor] 마이 페이지 상단 부분 css적용

--- final-project/src/Components/Mypage/MypageAccount.js
+++ final-project/src/Components/Mypage/MypageAccount.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import MypageModal from "./MypageModal";
 import { useDispatch, useSelector } from "react-redux";
 import { editUserProfileDB } from "../../redux/async/user";
+import mixin from "../../styles/Mixin";
 
 const MypageAccount = props => {
     const dispatch = useDispatch();
@@ -66,88 +67,38 @@ const MypageAccount = props => {
 
     return (
         <>
-            <Title>계정관리</Title>
+            <TitleWrapper>
+                <Title>계정관리</Title>
+            </TitleWrapper>
             <ControlContainer>
                 <ControlButton onClick={openModal}>학교 인증</ControlButton>
-                {modalVisible && (
-                    <MypageModal
-                        visible={modalVisible}
-                        closable={true}
-                        maskClosable={true}
-                        onClose={closeModal}
-                    />
-                )}
-
                 <ControlButton onClick={handleNicknameEditToggle}>
                     닉네임 설정
                 </ControlButton>
-                {isNicknameEditMode && (
-                    <TextField
-                        style={{ width: "500px", marginLeft: "10px" }}
-                        placeholder="변경할 닉네임을 입력해주세요"
-                        value={newNickname}
-                        onChange={handleNicknameChange}
-                        InputProps={{
-                            endAdornment: (
-                                <Button
-                                    style={{ width: "100px" }}
-                                    variant="outlined"
-                                    onClick={onNicknameChangeClick}
-                                >
-                                    변경하기
-                                </Button>
-                            ),
-                        }}
-                    />
-                )}
                 <ControlButton onClick={handleEmailEditToggle}>
                     로그인 이메일 변경
                 </ControlButton>
-                {isEmailEditMode && (
-                    <TextField
-                        style={{ width: "500px", marginLeft: "10px" }}
-                        placeholder="변경할 이메일을 입력해주세요"
-                        value={newEmail}
-                        onChange={handleEmailChange}
-                        InputProps={{
-                            endAdornment: (
-                                <Button
-                                    style={{ width: "100px" }}
-                                    variant="outlined"
-                                    onClick={onEmailChangeClick}
-                                >
-                                    변경하기
-                                </Button>
-                            ),
-                        }}
-                    />
-                )}
-
                 <ControlButton>비밀번호 설정</ControlButton>
             </ControlContainer>
-            <Title>기타</Title>
+            <TitleWrapper>
+                <Title>기타</Title>
+            </TitleWrapper>
             <DeleteAccountButton>탈퇴하기</DeleteAccountButton>
         </>
     );
 };
 
 const Title = styled.span`
-    font-size: 25px;
-    color: #707070;
-    margin: 50px 0 10px 0;
     display: block;
+    ${mixin.textProps(30, "extraBold", "gray2")}
 `;
-const ControlContainer = styled.div`
-    border: 1px solid;
-    color: #707070;
-    padding: 10px;
-`;
+const TitleWrapper = styled.div``;
+
+const ControlContainer = styled.div``;
 const ControlButton = styled.button`
-    display: block;
-    color: #707070;
-    background: none;
-    margin: 0 0 10px 10px;
-    font-size: 25px;
+    background-color: white;
+    ${mixin.outline("2px solid", "blue3")}
+    ${mixin.textProps(18, "semiBold", "gray3")}
 `;
 
 const DeleteAccountButton = styled.button`
