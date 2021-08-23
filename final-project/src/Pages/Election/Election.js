92f84292a6d5c1414467bd0ed08e7c56e08314a0
che-ri <rnjs1135@gmail.com>
2021-08-23 22:02:50 +0900

    [feat] 버튼이 셀렉되었을때 디테일 수정&추가하기 버튼 위치조정

--- final-project/src/Pages/Election/Election.js
+++ final-project/src/Pages/Election/Election.js
@@ -26,6 +26,8 @@ const Election = () => {
         moment().isAfter(post.end_date),
     );
     const currentList = isFinished ? finishedElectionList : ongoingElectionList;
+    const currentListName = isFinished ? "finished" : "ongoing";
+    // const currentList = isFinished ? finishedElectionList:ongoingElectionList
 
     //대학 인증을 한 사람만 볼 수 있도록 예외처리를 합니다.
     if (!user.univ_id || !user.country_id)
@@ -39,16 +41,20 @@ const Election = () => {
 
     return (
         <ElectionContainer>
-            <Title>
-                <h5>투표함</h5>
+            <Title>투표함</Title>
+            <Controls>
+                <Selecter selected={isFinished}>
+                    <button onClick={() => setIsFinished(false)}>
+                        진행중선거
+                    </button>
+                    <button onClick={() => setIsFinished(true)}>
+                        종료된선거
+                    </button>
+                </Selecter>
                 <DefaultButton onClick={() => history.push(`/election/write`)}>
                     추가하기
                 </DefaultButton>
-            </Title>
-            <Selecter selected={isFinished}>
-                <button onClick={() => setIsFinished(false)}>진행중선거</button>
-                <button onClick={() => setIsFinished(true)}>종료된선거</button>
-            </Selecter>
+            </Controls>
             {currentList && currentList.length < 1 ? (
                 <Message
                     message="아직 투표가 없습니다"
@@ -58,22 +64,43 @@ const Election = () => {
             ) : (
                 <>
                     <GridContainer>
-                        {currentList.map(ele => (
-                            <Post
-                                key={ele.election_id}
-                                isVoted={ele.votes.length > 0}
-                                onClick={() =>
-                                    history.push(
-                                        `/election/detail/${ele.election_id}`,
-                                    )
-                                }
-                            >
-                                <span>{ele.name}</span>
-                                {ele.votes.length > 0 && (
-                                    <VotingComplete>투표 완료!</VotingComplete>
-                                )}
-                            </Post>
-                        ))}
+                        {currentListName === "ongoing"
+                            ? currentList.map(ele => (
+                                  <OngoingPost
+                                      key={ele.election_id}
+                                      isVoted={ele.votes.length > 0}
+                                      onClick={() =>
+                                          history.push(
+                                              `/election/detail/${ele.election_id}`,
+                                          )
+                                      }
+                                  >
+                                      <span>{ele.name}</span>
+                                      {ele.votes.length > 0 && (
+                                          <VotingComplete>
+                                              투표 완료!
+                                          </VotingComplete>
+                                      )}
+                                  </OngoingPost>
+                              ))
+                            : currentList.map(ele => (
+                                  <FinishedPost
+                                      key={ele.election_id}
+                                      isVoted={ele.votes.length > 0}
+                                      onClick={() =>
+                                          history.push(
+                                              `/election/detail/${ele.election_id}`,
+                                          )
+                                      }
+                                  >
+                                      <span>{ele.name}</span>
+                                      {ele.votes.length > 0 && (
+                                          <VotingComplete>
+                                              투표 완료!
+                                          </VotingComplete>
+                                      )}
+                                  </FinishedPost>
+                              ))}
                     </GridContainer>
                 </>
             )}
@@ -85,12 +112,15 @@ const ElectionContainer = styled.div``;
 
 const Title = styled.div`
     ${mixin.outline("1px solid", "gray4", "bottom")};
-    ${mixin.flexBox("space-between", "flex-end")};
     padding-bottom: 10px;
     margin-bottom: 15px;
-    h5 {
-        ${mixin.textProps(30, "extraBold", "black")};
-    }
+    ${mixin.textProps(30, "extraBold", "black")};
+`;
+
+const Controls = styled.div`
+    ${mixin.flexBox("space-between", "flex-end")};
+    padding-bottom: 15px;
+    ${mixin.outline("1px solid", "gray4", "bottom")};
 `;
 
 const Selecter = styled.div`
@@ -100,21 +130,38 @@ const Selecter = styled.div`
         text-align: center;
         margin-right: 7px;
         border-radius: 16px;
-        ${mixin.textProps(18, "semiBold", "gray1")};
         background-color: ${props => props.theme.color.white};
         :first-child {
+            margin-right: 15px;
+            transition: all 0.3s ease;
+            ${props =>
+                props.selected
+                    ? mixin.textProps(18, "semiBold", "gray3")
+                    : mixin.textProps(18, "semiBold", "gray1")}
+            ${props =>
+                props.selected
+                    ? `box-shadow:  0 5px 5px -4px #cdcdcd;`
+                    : `box-shadow: inset -2px 5px 5px -5px #cdcdcd;`}
             ${props =>
                 mixin.outline(
                     "2px solid",
                     !props.selected ? "mainMint" : "blue2",
                 )}
-            margin-right: 15px;
         }
         :last-child {
+            transition: all 0.3s ease;
+            ${props =>
+                props.selected
+                    ? mixin.textProps(18, "semiBold", "gray1")
+                    : mixin.textProps(18, "semiBold", "gray3")}
+            ${props =>
+                props.selected
+                    ? `box-shadow: inset -2px 5px 5px -4px #cdcdcd;`
+                    : `box-shadow:  0 5px 5px -6px #cdcdcd;`}
             ${props =>
                 mixin.outline(
                     "2px solid",
-                    props.selected ? "mainMint" : "blue2",
+                    props.selected ? "mainMint" : "blue3",
                 )}
         }
     }
@@ -128,20 +175,44 @@ const GridContainer = styled.div`
     gap: 30px 25px;
 `;
 
-const Post = styled.div`
+const OngoingPost = styled.div`
     overflow: hidden;
     border-radius: 50px;
     cursor: pointer;
-    padding: 20px;
+    padding: 20px 25px;
     ${mixin.outline("3px solid", "blue2")}
     ${mixin.flexBox("center", "center", null, "100px")};
     ${mixin.floatBox("relative")}
     ${props =>
         props.isVoted
             ? mixin.outline("3px solid", "gray1")
-            : mixin.outline("3px solid", "blue2")}
+            : mixin.outline("3px solid", "blue2")};
+    span {
+        ${mixin.textProps(20, "regular", "gray1")}
+        ${mixin.textboxOverflow(2)}
+    }
+`;
+
+const FinishedPost = styled.div`
+    overflow: hidden;
+    border-radius: 50px;
+    cursor: pointer;
+    padding: 20px 25px;
+    background: ${({ theme }) => theme.color.mainGray};
+    ${mixin.outline("3px solid", "gray3")}
+    ${mixin.flexBox("center", "center", null, "100px")};
+    ${mixin.floatBox("relative")}
+    ${props => (props.isVoted ? mixin.outline("3px solid", "gray2") : "")}
+
+    :hover {
+        background: ${({ theme }) => theme.color.white};
+        span {
+            ${mixin.textProps(20, "regular", "gray1")}
+        }
+    }
     span {
         ${mixin.textboxOverflow(2)}
+        ${mixin.textProps(20, "regular", "gray2")}
     }
 `;
 
