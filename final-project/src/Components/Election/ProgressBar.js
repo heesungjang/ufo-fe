70f9b25273ff9ba5316ccff82664249774b79c1d
che-ri <rnjs1135@gmail.com>
2021-08-24 00:17:12 +0900

    [fix] 프로그래스바가 100%이상되면 더이상 안 늘어나게 수정

--- final-project/src/Components/Election/ProgressBar.js
+++ final-project/src/Components/Election/ProgressBar.js
@@ -10,7 +10,10 @@ import moment from "moment";
 
 const ProgressBar = ({ start, end }) => {
     const max = moment(end).diff(start); //최대값
-    const value = Math.ceil((moment().diff(moment(start)) / max) * 100); //현재값을 백분율로 나타낸것
+    const value =
+        Math.ceil((moment().diff(moment(start)) / max) * 100) >= 100
+            ? 100
+            : Math.ceil((moment().diff(moment(start)) / max) * 100); //현재값을 백분율로 나타낸것
 
     // 투표가 시작전이면 value 값은 0과 같거나 작게 됩니다. 따라서, value가 0보다 크면 프로그래스바를 진행하고, 아니면 진행하지 않습니다.
     return (
@@ -30,6 +33,7 @@ const Container = styled.div`
     width: 100%;
     height: 10px;
     background-color: ${({ theme }) => theme.color.blue2};
+    overflow: hidden;
 `;
 
 const Bar = styled.div`
