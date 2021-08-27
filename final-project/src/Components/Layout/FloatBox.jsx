c09c3c3bc8399153cc7a0467819ab63db069aba7
che-ri <rnjs1135@gmail.com>
2021-08-28 08:12:11 +0900

    [feat] 자유게시판, 학교게시판일때 플루팅버튼에 작성버튼 달기

--- final-project/src/Components/Layout/FloatBox.jsx
+++ final-project/src/Components/Layout/FloatBox.jsx
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import mixin from "../../Styles/Mixin";
 import theme from "../../Styles/theme";
+import { useLocation } from "react-router";
+import { history } from "../../Redux/configureStore";
 
+//아이콘
+import { GrEdit } from "react-icons/gr";
 import { BiArrowToTop } from "react-icons/bi";
 
 const FloatBox = () => {
+    const { pathname } = useLocation();
+    const [isWriteBntOn, setIsWriteBntOn] = useState(false); //작성버튼을 보여줄지 말지에 대한 판별값, 자유게시판, 국가게시판이면 글쓰기버튼이 생긴다.
     const [isVisible, setIsVisible] = useState(false); //버튼을 보여줄지 말지에 대한 판별값
+    const isDesktop =
+        document.documentElement.clientWidth >= 1080 ? true : false;
+
+    const goToWrite = () => {
+        //pathname이 freeboard면 자유게시판작성페이지로, 아니면 대학게시판작성페이지로 보낸다.
+        if (pathname === "/freeboard") return history.push("/freeboard/write");
+        history.push("/univboard/write");
+    };
 
     const scrollToTop = () => {
         //스크롤을 위로 올리는 함수
@@ -17,22 +31,32 @@ const FloatBox = () => {
     };
 
     useEffect(() => {
-        // Button is displayed after scrolling for 500 pixels
         const toggleVisibility = () => {
+            //토글을 보여줄지 말지를 결정하는 함수입니다.
             if (window.pageYOffset > 0) {
                 setIsVisible(true);
             } else {
                 setIsVisible(false);
             }
         };
-
         window.addEventListener("scroll", toggleVisibility);
-
         return () => window.removeEventListener("scroll", toggleVisibility);
     }, []);
 
+    useEffect(() => {
+        //대학게시판이나, 자유게시판페이지면 isWriteBntOn을 true로 바꿔서 글쓰기 플루팅버튼을 보이게 합니다.
+        if (pathname === "/univboard" || pathname === "/freeboard")
+            setIsWriteBntOn(true);
+        else setIsWriteBntOn(false);
+    }, [pathname]);
+
     return (
         <Container>
+            {!isDesktop && isWriteBntOn && (
+                <Button onClick={goToWrite}>
+                    <GrEdit />
+                </Button>
+            )}
             {isVisible && (
                 <Button onClick={scrollToTop}>
                     <BiArrowToTop />
@@ -48,10 +72,11 @@ const Container = styled.div`
         null,
         `${theme.calRem(100)}`,
         `${theme.calRem(100)}`,
-    )}
+    )};
+    ${mixin.flexBox(null, null, "column", null)};
 
     @media ${({ theme }) => theme.mobile} {
-        right: ${theme.calRem(20)};
+        right: ${theme.calRem(30)};
         bottom: ${theme.calRem(100)};
     }
 `;
@@ -73,6 +98,12 @@ const Button = styled.button`
     }
     svg {
         vertical-align: middle;
+        path {
+            stroke: ${({ theme }) => theme.color.gray3};
+            :hover {
+                stroke: ${({ theme }) => theme.color.gray2};
+            }
+        }
     }
 
     @media ${({ theme }) => theme.mobile} {
