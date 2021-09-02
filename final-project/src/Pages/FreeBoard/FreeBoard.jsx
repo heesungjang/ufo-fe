67b5c933180c952c7cc5bcf8c651a65b49d92d5a
Heesung jang <heesungj7@gmail.com>
2021-09-02 23:56:15 +0900

    [refactor] 좋아요 버튼 색상 변경 / 콘솔 제거

--- final-project/src/Pages/FreeBoard/FreeBoard.jsx
+++ final-project/src/Pages/FreeBoard/FreeBoard.jsx
@@ -94,7 +94,6 @@ const FreeBoard = () => {
     // 초기 페이지 진입시 게시물 요청
 
     useEffect(() => {
-        console.log("hi");
         dispatch(getFreeListDB(postListQueryData));
     }, [dispatch, page, selectedTag, selectedCountry]);
 
